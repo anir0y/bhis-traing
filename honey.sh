131e4f2e57d92292ee07b64cd4918f7351275a11
Animesh Roy <animeshroy@live.com>
2022-01-26 01:33:28 +0530

    Create honey.sh

---
+++ honey.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# author : @anir0y
+# learning during class how to block active IP connection
+echo "HoneyPot Running"
+
+while [ 1 ]
+do
+        IP=`nc -lvl 8080 2>&1> /dev/null | grep from | awk -F '[] []' '{print $3;}'`
+        echo $P
+        iptables -A INPUT -p tcp -s $IP -j DROP
+        echo $IP has been blocked
+        echo "====Summery===="
+        sudo iptables -L  | head -n 5
+        sleep 10 
+        echo "Resetting Rules"
+        iptables -F
+done
